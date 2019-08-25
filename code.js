c75e478d577f31de33e70950b6ea96687d1220da
davejang <djunkaroo621@users.noreply.github.com>
2019-08-25 15:53:12 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,132 +1,52 @@
-
-const question = document.getElementById("question");
+const inputtedChore = document.getElementById("inputtedChore");
+const subButton = document.getElementById("subButton");
 const compiledList = document.getElementById("compiledList");
-const mainDiv = document.getElementById("leest");
 
+let toDoArray= [];
 
 function hitSubmit() {
-const value = question.value;
 
-if (value) {
-  localStorage.setItem(value, value);
-  location.reload();
+const toDoValue = inputtedChore.value;
+
+toDoArray.push(toDoValue);
+console.log(toDoArray);
+inputtedChore.innerHTML = localStorage.setItem("toDoKey", toDoArray);
+compiledList.innerHTML = localStorage.getItem("toDoKey", toDoArray);
 }
 
-}
 
-for (let i = 0; i < localStorage.length; i++) {
-
-const key = localStorage.key(i);
-const value = localStorage.getItem(key);
-
-var list = document.createElement("P");
-
-var finishedButton = document.createElement("input");
-finishedButton.setAttribute('type','radio');
-finishedButton.setAttribute('name','finished');
-
-var delButton = document.createElement("input");
-delButton.setAttribute('type','radio');
-delButton.setAttribute('name','delete');
-
-compiledList.appendChild(list);
-compiledList.appendChild(finishedButton);
-compiledList.appendChild(delButton);
-
-compiledList.innerHTML += value;
-}
-// compiledList.style.textDecoration="line-through";
-// }
-
-// onclick='this.style.textDecoration="line-through"'
-// document.querySelector("#compiledList").style.textDecoration= "line-through";
-
+// BRAINSTORMING CODE
+// const chore = document.getElementById("inputtedChore");
+// const compiledList = document.getElementById("compiledList");
+// const mainDiv = document.getElementById("leest");
 //
-// document.getElementById("compiledList").innerHTML = "<del>turtle</del>";
+// function hitSubmit() {
+// const value = inputtedChore.value;
 //
-// }
-
-
-// function strikeThrough() {
-//
-// compiledList.changeStyle
-//
+// if (value) {
+//   localStorage.setItem(value, value);
+//   location.reload();
+//   }
 // }
 //
+// for (let i = 0; i < localStorage.length; i++) {
 //
-// compiledList.addEventListener("click",);
-
-//brainstorm code below:
-// document.getElementById("compiledList").innerHTML = mav;
-
-
+// const key = localStorage.key(i);
+// const value = localStorage.getItem(key);
 //
-// localStorage.setItem("chore", choreX);
-// let mav = localStorage.getItem("chore");
-// document.getElementById("compiledList").innerHTML = mav;
-
-
-
-
-// document.getElementById("compiledList").innerHTML = choreX;
-// localStorage.setItem("chore", choreX);
-// for(let i = 0; i <= localStorage.length; i++) {
-//   localStorage.setItem("chore", choreX);
+// var list = document.createElement("P");
 //
+// var finishedButton = document.createElement("input");
+// finishedButton.setAttribute('type','radio');
+// finishedButton.setAttribute('id','finish');
+//
+// var delButton = document.createElement("input");
+// delButton.setAttribute('type','radio');
+//
+// compiledList.appendChild(list);
+// compiledList.appendChild(finishedButton);
+// compiledList.appendChild(delButton);
+//
+// compiledList.innerHTML += value;
 // }
-
-
-
-// var listElement = document.createElement("li");
-// var goober = document.getElementById("compiledList");
-// }
-//
-// var listElement = document.createElement("li");
-// var goober = document.getElementById("compiledList");
-// goober.appendChild(listElement);
-// document.getElementById("compiledList").innerHTML=choreX;
-
-
-
-//
-// var x = localStorage.getItem('chore');
-// document.getElementById("leest").innerHTML = x;
-
-
-
-
-// function listFunc() {
-//   var para = document.createElement("P");
-//   para.innerText = x;
-//   document.body.appendChild(para);
-// }
-
-
-// localStorage.getItem("chore", choreX);
-// document.getElementById('textbox').innerHTML = key:"chore";
-
-
-
-// var y = (document.getElementById("compiledList").innerHTML = "What's first?");
-// console.log(y);
-
-// var x = document.getElementById("compiledList").value;
-// console.log(x);
-// var x = document.getElementById("compiledList").innerHTML;
-// localStorage.setItem('chore', x);
-
-
-// let newVar;
-// newVar = document.getElementById("compiledList").innerHTML;
-// localStorage.setItem("chore", newVar);
-
-// localStorage.setItem(choreX, choreX);
-//
-// var toDo = localStorage.getItem(choreX);
-// document.getElementById("compiledList").innerHTML = toDo;
-
-
-//brainstorming code
-// localStorage.setItem("chore", choreX);
-//
 // document.getElementById("compiledList").innerHTML = choreX;
