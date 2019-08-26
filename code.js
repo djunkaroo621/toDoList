fb41c7a1b5e3b018231fad748423cb8d52a2c9b5
davejang <djunkaroo621@users.noreply.github.com>
2019-08-26 11:51:23 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -17,10 +17,8 @@ inputtedChore.innerHTML= localStorage.setItem("toDoKey", strungList);
 
 let parsedList= JSON.parse(strungList);
 
-//something is buggy below this comment that is making the output get progressively longer instead of being a distinct todo list item. Fixed- Refer to next comment.
 for (let i= 0; i< localStorage.length; i++) {
 
-//the single line of code directly below took approx. 4-5 hours to think of.
   toDoArray.shift(toDoValue);
   const value= localStorage.getItem(i);
   var appender= document.createElement("P");
@@ -28,7 +26,7 @@ for (let i= 0; i< localStorage.length; i++) {
   finishedButton.setAttribute('type','checkbox');
   finishedButton.setAttribute('id','finishedButton');
   finishedButton.setAttribute('onclick',strike());
-  
+
   compiledList.appendChild(appender);
   compiledList.appendChild(finishedButton);
   compiledList.innerHTML += parsedList;
@@ -37,39 +35,3 @@ for (let i= 0; i< localStorage.length; i++) {
     document.getElementById("compiledList").style.textDecoration= "line-through";
   }
 }
-
-
-
-
-
-// BRAINSTORMING CODE
-// function hitSubmit() {
-// const value = inputtedChore.value;
-//
-// if (value) {
-//   localStorage.setItem(value, value);
-//   location.reload();
-//   }
-// }
-//
-// for (let i = 0; i < localStorage.length; i++) {
-//
-// const key = localStorage.key(i);
-// const value = localStorage.getItem(key);
-//
-// var list = document.createElement("P");
-//
-// var finishedButton = document.createElement("input");
-// finishedButton.setAttribute('type','radio');
-// finishedButton.setAttribute('id','finish');
-//
-// var delButton = document.createElement("input");
-// delButton.setAttribute('type','radio');
-//
-// compiledList.appendChild(list);
-// compiledList.appendChild(finishedButton);
-// compiledList.appendChild(delButton);
-//
-// compiledList.innerHTML += value;
-// }
-// document.getElementById("compiledList").innerHTML = choreX;
