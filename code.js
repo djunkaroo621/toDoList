34c6241d42a70dc01f9c94faa9dbc0e93f33d7b3
davejang <djunkaroo621@users.noreply.github.com>
2019-09-09 20:27:17 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,65 +1,76 @@
-var inputtedChoreVar= document.getElementById("inputtedChore");
-var toDoListVar= document.getElementById("toDoList");
+var inputtedChoreVar = document.getElementById("inputtedChore");
+var toDoListVar = document.getElementById("toDoList");
 
-var toDoArray= [];
+var toDoArray = [];
 
-document.getElementById('subButton').addEventListener('click',hitSubmit);
+document.getElementById('subButton').addEventListener('click', hitSubmit);
 
 function hitSubmit() {
 
-  let x= inputtedChoreVar.value;
+  if (inputtedChoreVar.value.length <= 0) {
+    newListElement= false;
+}
+
+  let x = inputtedChoreVar.value;
   toDoArray.push(x);
   console.log(toDoArray);
 
-  localStorage.setItem('toDoKey', toDoArray);
+  localStorage.setItem('toDoKey', JSON.stringify(toDoArray));
 
-  inputtedChoreVar.value= '';
+  inputtedChoreVar.value = "";
 
-  var newListElement= document.createElement('LI');
-  var newTextNode= document.createTextNode(x);
+  let newListElement = document.createElement('LI');
+  let newTextNode = document.createTextNode(x);
   newListElement.appendChild(newTextNode);
   toDoListVar.appendChild(newListElement);
 
-  var fButtonElement= document.createElement('input');
-  fButtonElement.setAttribute('id','fButton');
-  fButtonElement.setAttribute('type','checkbox');
-  fButtonElement.setAttribute('value','FINISHED');
+  let fButtonElement = document.createElement('input');
+  fButtonElement.setAttribute('id', 'fButton');
+  fButtonElement.setAttribute('type', 'checkbox');
+  fButtonElement.setAttribute('value', 'FINISHED');
   newListElement.append(fButtonElement);
 
-// Got help on stackoverflow below this comment for the
-// fButtonElement function- the help was
-// putting the proper argument in the function. Didn't occur to me that
-// I could manipulate the appended list item 'newListElement.'
-// Using the template for the line-through button, I was able to code the
-// delete button on my own.
-  fButtonElement.onclick= function() {
-    strike(newListElement, fButtonElement);
-  } // function that calls "strike" with an argument
+  // Got help on stackoverflow below this comment for the
+  // fButtonElement function- the help was
+  // putting the proper argument in the function. Didn't occur to me that
+  // I could manipulate the appended list item 'newListElement.'
+  // Using the template for the line-through button, I was able to code the
+  // delete button on my own.
 
-  var delButtonElement= document.createElement('input');
-  delButtonElement.setAttribute('id','dButton');
-  delButtonElement.setAttribute('type','button');
-  delButtonElement.setAttribute('value','DELETE');
+
+  // function that calls "strike" with an argument
+
+  fButtonElement.onclick = function() {
+    strike(newListElement, fButtonElement);
+    }
+
+  var delButtonElement = document.createElement('input');
+  delButtonElement.setAttribute('id', 'dButton');
+  delButtonElement.setAttribute('type', 'button');
+  delButtonElement.setAttribute('value', 'DELETE');
   newListElement.appendChild(delButtonElement);
-  delButtonElement.onclick= function() {
+  delButtonElement.onclick = function() {
     trashDunk(newListElement);
   }
-  document.getElementById("universalDelete").onclick= function() {
+
+  document.getElementById("universalDelete").onclick = function() {
     univDel();
   }
 }
-// Realized that fButtonElement must be an argument to make unchecking work properly.
-function strike(newListElement, fButtonElement) {
 
-  if (fButtonElement.checked) {newListElement.style.textDecoration='line-through';
-} else {newListElement.style.textDecoration='none'}
+// Realized that fButtonElement must be an argument to make *unchecking* work properly.
+function strike(newListElement, fButtonElement) {
+  if (fButtonElement.checked) {
+    newListElement.style.textDecoration = 'line-through';
+  } else {
+    newListElement.style.textDecoration = 'none'
+  }
 }
+
 function trashDunk(newListElement) {
   newListElement.parentNode.removeChild(newListElement);
 }
+
 function univDel() {
-  var punt= document.getElementById("toDoList");
-  punt.innerHTML='';
-
-
-}
+  document.getElementById("toDoList").innerHTML = '';
+  }
