e53147996f3b574ddfc3f81666414484e76bf021
davejang <djunkaroo621@users.noreply.github.com>
2019-09-12 11:06:32 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,15 +1,11 @@
 var inputtedChoreVar = document.getElementById('inputted-chore');
 var toDoListVar = document.getElementById('to-do-list');
-
 var toDoArray = [];
-
 document.getElementById('sub-button').addEventListener('click', hitSubmit);
-
 function hitSubmit() {
   if (inputtedChoreVar.value.length === 0) {
     return;
   }
-
   let x = inputtedChoreVar.value;
   toDoArray.push(x);
   console.log(toDoArray);
@@ -21,19 +17,12 @@ function hitSubmit() {
   let newListItem = document.createElement('LI');
   newListItem.setAttribute('id', 'new-list-element');
   let newTextNode = document.createTextNode(x);
-
   function createList() {
     newListItem.appendChild(newTextNode);
     toDoListVar.appendChild(newListItem);
   }
-
   createList();
-  createFinishButton(newListItem);
-  createDeleteButton(newListItem);
-  createEmptyButton(newListItem);
-
   let finishButton = document.createElement('input');
-
   function createFinishButton() {
     finishButton.setAttribute('id', 'finish-checkbox');
     finishButton.setAttribute('type', 'checkbox');
@@ -41,39 +30,36 @@ function hitSubmit() {
     newListItem.append(finishButton);
   }
 
+  createFinishButton();
+
   finishButton.onclick = function() {
     strikeListItem(newListItem, finishButton);
   }
-
-  function strikeListItem(newListItem, finishButton) {
-    if (finishButton.checked) {
-      newListItem.style.textDecoration = 'line-through';
-    } else {
-      newListItem.style.textDecoration = 'none'
-    }
-
-    strikeListItem();
-
-  function createDeleteButton(newListItem) {
-    var deleteButton = document.createElement('input');
+  let deleteButton = document.createElement('input');
+  function createDeleteButton() {
     deleteButton.setAttribute('id', 'delete-button');
     deleteButton.setAttribute('type', 'button');
     deleteButton.setAttribute('value', 'DELETE');
     newListItem.appendChild(deleteButton);
     deleteButton.onclick = function() {
-      redDeleteItem(newListItem) {
-        newListItem.parentNode.removeChild(newListItem);
-      }
+      redDeleteItem(newListItem);
     }
   }
-
-    createDeleteButton();
-
+  createDeleteButton();
   function createEmptyButton() {
     document.getElementById('empty-list').onclick = function emptyList() {
       document.getElementById('to-do-list').innerHTML = '';
     }
   }
-
-    createEmptyButton();
+  createEmptyButton();
+}
+function strikeListItem(newListItem, finishButton) {
+  if (finishButton.checked) {
+    newListItem.style.textDecoration = 'line-through';
+  } else {
+    newListItem.style.textDecoration = 'none'
   }
+}
+function redDeleteItem(newListItem) {
+  newListItem.parentNode.removeChild(newListItem);
+}
