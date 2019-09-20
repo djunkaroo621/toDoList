0f2ca83147cec6806d1e8f1c5b28cfc2c2b0ae6d
davejang <djunkaroo621@users.noreply.github.com>
2019-09-19 21:07:58 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -11,7 +11,7 @@ function hitSubmit() {
         return;
     }
 
-// Creates the list
+    // Creates the list
     const x = inputtedChoreVar.value;
     toDoArray.push(x);
     console.log(toDoArray);
@@ -25,35 +25,35 @@ function hitSubmit() {
 
     createList(newListItem, toDoListVar, newTextNode);
 
-// Creates the finish button and executes the line-through feature (checkbox)
+    // Creates the finish button and executes the line-through feature (checkbox)
     const finishButton = document.createElement('input');
 
     createFinishButton(newListItem, finishButton);
 
-    finishButton.onclick = function() {
+    finishButton.onclick = function () {
         strikeListItem(newListItem, finishButton);
     }
 
-// Function call that creates the delete button
+    // Function call that creates the delete button
     const deleteButton = document.createElement('input');
 
     createDeleteButton(deleteButton, newListItem);
 
-// Function call that creates the empty list button
+    // Function call that creates the empty list button
     createEmptyButton();
 }
 
 // Function executes the line through feature
-const strikeListItem = (newListItem,finishButton) => {
+const strikeListItem = (newListItem, finishButton) => {
     finishButton.checked ? newListItem.style.textDecoration = 'line-through' :
-    newListItem.style.textDecoration = 'none'
-    }
+        newListItem.style.textDecoration = 'none'
+}
 
 // Function executes creating a todo list item
 const createList = (newListItem, toDoListVar, newTextNode) => {
     newListItem.appendChild(newTextNode);
     toDoListVar.appendChild(newListItem);
-    }
+}
 
 // Function creates the checkbox
 const createFinishButton = (newListItem, finishButton) => {
@@ -61,7 +61,7 @@ const createFinishButton = (newListItem, finishButton) => {
     finishButton.setAttribute('type', 'checkbox');
     finishButton.setAttribute('value', 'FINISHED');
     newListItem.append(finishButton);
-    }
+}
 
 // Function creates the red delete button
 const createDeleteButton = (deleteButton, newListItem) => {
@@ -71,8 +71,8 @@ const createDeleteButton = (deleteButton, newListItem) => {
     newListItem.appendChild(deleteButton);
     deleteButton.onclick = function () {
         redDeleteItem(newListItem);
-        }
     }
+}
 
 // Function executes the delete feature when pressing the red delete button
 const redDeleteItem = (newListItem) => newListItem.parentNode.removeChild(newListItem);
